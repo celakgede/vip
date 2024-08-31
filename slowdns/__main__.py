7c70e06b465f9c23c887657bcf7992ee4336a03b
celakgede <vpnrmbl1@gmail.com>
2024-08-31 23:00:40 +0800

    Delete slowdns/__main__.py

--- slowdns/__main__.py
+++
@@ -1,8 +0,0 @@
-from agin import *
-from importlib import import_module
-from agin.modules import ALL_MODULES
-for module_name in ALL_MODULES:
-        imported_module = import_module("agin.modules." + module_name)
-if __name__ == "__main__":
-	bot.loop.run_until_complete(sbot())
-	aapp.run()

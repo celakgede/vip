5be56b358a29ca2b30c78fc3ebd9ca1d7180981e
celakgede <vpnrmbl1@gmail.com>
2024-09-01 21:30:02 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -17,21 +17,24 @@ sleep 0.5
 green "                   Proses dimulai..."
 sleep 0.5
 
-# Update repository dan sistem
-apt update -y
-apt upgrade -y
-apt dist-upgrade -y
-apt install sudo -y
+# Mengatur environment variable untuk menghindari prompt interaktif
+export DEBIAN_FRONTEND=noninteractive
 
-# Instalasi untuk Debian dan Ubuntu
+# Update repository dan sistem
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt install sudo -y
+
+# Instalasi untuk Ubuntu 
 sudo apt-get clean all
-apt install -y debconf-utils
-apt-get remove --purge ufw firewalld -y
-apt-get remove --purge exim4 -y
-apt-get autoremove -y
-apt install -y --no-install-recommends software-properties-common
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+sudo apt install -y debconf-utils
+sudo apt-get remove --purge ufw firewalld -y
+sudo apt-get remove --purge exim4 -y
+sudo apt-get autoremove -y
+sudo apt install -y --no-install-recommends software-properties-common
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install iptables iptables-persistent netfilter-persistent figlet ruby libxml-parser-perl squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch lsof openssl openvpn easy-rsa fail2ban tmux stunnel4 squid3 dropbear socat cron bash-completion ntpdate xz-utils apt-transport-https gnupg2 dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd apt git speedtest-cli p7zip-full libjpeg-dev zlib1g-dev python python3 python3-pip shc build-essential nodejs nginx php php-fpm php-cli php-mysql p7zip-full
 
 # remove unnecessary files
@@ -41,27 +44,27 @@ sudo apt-get -y --purge remove samba* >/dev/null 2>&1
 sudo apt-get -y --purge remove apache2* >/dev/null 2>&1
 sudo apt-get -y --purge remove bind9* >/dev/null 2>&1
 sudo apt-get -y remove sendmail* >/dev/null 2>&1
-apt autoremove -y >/dev/null 2>&1
+sudo apt autoremove -y >/dev/null 2>&1
 
 # finishing
-apt install python3 python3-pip git speedtest-cli -y
+sudo apt install python3 python3-pip git speedtest-cli -y
 sudo apt-get install -y p7zip-full
-pip3 install telethon==1.24.0
-pip3 install requests==2.25.1
-apt install at -y
+sudo pip3 install telethon==1.24.0
+sudo pip3 install requests==2.25.1
+sudo apt install at -y
 sudo apt-get -y install vnstat
-/etc/init.d/vnstat restart
+sudo /etc/init.d/vnstat restart
 sudo apt-get -y install libsqlite3-dev
-wget -q  ${REPOSC}/vnstat-2.6.tar.gz
+wget -q ${REPOSC}/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
-./configure --prefix=/usr --sysconfdir=/etc && make && make install
+./configure --prefix=/usr --sysconfdir=/etc && make && sudo make install
 cd
-vnstat -u -i $NET
-sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
-chown vnstat:vnstat /var/lib/vnstat -R
-systemctl enable vnstat
-/etc/init.d/vnstat restart
+sudo vnstat -u -i $NET
+sudo sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+sudo chown vnstat:vnstat /var/lib/vnstat -R
+sudo systemctl enable vnstat
+sudo /etc/init.d/vnstat restart
 rm -f /root/vnstat-2.6.tar.gz
 rm -rf /root/vnstat-2.6
 

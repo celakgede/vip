9d7b5dfe8d337bdd1fbdf66d52a7f15a24802e75
celakgede <vpnrmbl1@gmail.com>
2025-08-07 21:21:14 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,68 +1,47 @@
 #!/bin/bash
 clear
+REPOSC="https://raw.githubusercontent.com/celakgede/vip/main"
 red='\e[1;31m'
 green2='\e[1;32m'
 yell='\e[1;33m'
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
-
-echo "           Tools install...!"
-echo "                  Progress..."
-sleep 0.5
+W='\e[1;37m'
+echo -e " ${W}Menginstall tools !"
+echo -e " Proses dimulai...${NC}"
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt install sudo -y
 sudo apt-get clean all
+apt install -y debconf-utils
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
 apt-get autoremove -y
-apt-get --assume-yes install iptables
-apt-get --assume-yes install iptables-persistent 
-apt-get --assume-yes install netfilter-persistent
-apt-get --assume-yes install figlet
-apt-get --assume-yes install ruby 
-apt-get --assume-yes install libxml-parser-perl 
-apt-get --assume-yes install squid 
-apt-get --assume-yes install nmap 
-apt-get --assume-yes install rsyslog 
-apt-get --assume-yes install iftop 
-apt-get --assume-yes install htop 
-apt-get --assume-yes install zip 
-apt-get --assume-yes install unzip 
-apt-get --assume-yes install net-tools 
-apt-get --assume-yes install sed 
-apt-get --assume-yes install bc 
-apt-get --assume-yes install apt-transport-https
-apt-get ---assume-yes install build-essential 
-apt-get --assume-yes install libxml-parser-perl 
-apt-get --assume-yes install neofetch 
-apt-get --assume-yes install lsof 
-apt-get --assume-yes install openssl 
-apt-get --assume-yes install openvpn 
-apt-get --assume-yes install easy-rsa 
-apt-get --assume-yes install fail2ban 
-apt-get --assume-yes install tmux 
-apt-get --assume-yes install stunnel4 
-apt-get --assume-yes install squid3 
-apt-get --assume-yes install socat 
-apt-get --assume-yes install cron 
-apt-get --assume-yes install bash-completion 
-apt-get --assume-yes install ntpdate 
-apt-get --assume-yes install apt-transport-https
-apt-get --assume-yes install chrony 
-apt-get --assume-yes install speedtest-cli
-apt-get --assume-yes install p7zip-full 
-apt-get --assume-yes install python3 
-apt-get --assume-yes install python3-pip 
-apt-get --assume-yes install shc 
-apt-get --assume-yes install nodejs
-apt-get --assume-yes install nginx 
-apt-get --assume-yes install php 
-apt-get --assume-yes install php-fpm
-apt-get --assume-yes install php-cli 
-apt-get --assume-yes install dropbear
+apt install -y --no-install-recommends software-properties-common
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install iptables iptables-persistent netfilter-persistent figlet ruby libxml-parser-perl squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch lsof openssl openvpn easy-rsa fail2ban tmux stunnel4 squid3 dropbear socat cron bash-completion ntpdate xz-utils apt-transport-https gnupg2 dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd apt git speedtest-cli p7zip-full libjpeg-dev zlib1g-dev python python3 python3-pip shc build-essential nodejs nginx php php-fpm php-cli php-mysql p7zip-full
 
+# remove unnecessary files
+sudo apt-get autoclean -y >/dev/null 2>&1
+sudo apt-get -y --purge remove unscd >/dev/null 2>&1
+sudo apt-get -y --purge remove samba* >/dev/null 2>&1
+sudo apt-get -y --purge remove apache2* >/dev/null 2>&1
+sudo apt-get -y --purge remove bind9* >/dev/null 2>&1
+sudo apt-get -y remove sendmail* >/dev/null 2>&1
+apt autoremove -y >/dev/null 2>&1
+# finishing
+apt install python3 python3-pip git speedtest-cli -y
+sudo apt-get install -y p7zip-full
+pip3 install telethon==1.24.0
+pip3 install requests==2.25.1
+apt install at -y
 sudo apt-get -y install vnstat
 /etc/init.d/vnstat restart
 sudo apt-get -y install libsqlite3-dev
-wget https://raw.githubusercontent.com/celakgede/vip/main/vnstat-2.6.tar.gz
+wget -q  ${REPOSC}/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install
@@ -75,12 +54,6 @@ systemctl enable vnstat
 rm -f /root/vnstat-2.6.tar.gz
 rm -rf /root/vnstat-2.6
 
-# // install lolcat
-wget https://raw.githubusercontent.com/celakgede/vip/install/main/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
-
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-yellow "Dependencies successfully installed..."
+green "Dependencies successfully installed..."
 sleep 1
 clear
-
-mkdir -p /etc/tools
